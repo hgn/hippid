5d5fb8067252ef10bcce7d96654d6e88f45fa087
Hagen Paul Pfeifer <hagen@jauu.net>
2018-09-21 22:26:48 +0200

    initial commit
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

---
+++ run.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import os
+import sys
+import datetime
+import json
+import random
+import argparse
+import asyncio
+import time
+import tempfile
+import shutil
+import glob
+
+from aiohttp import web
+
+from httpd.utils import log
+from handler import api_ping
+from handler import generator
+
+
+
+try:
+    import pympler.summary
+    import pympler.muppy
+except:
+    pympler = None
+
+
+
+APP_VERSION = "001"
+
+# exit codes for shell, failures can later be sub-devided
+# if required and shell/user has benefit of this information
+EXIT_OK      = 0
+EXIT_FAILURE = 1
+
+
+
+def set_config_defaults(app):
+    # CAN be overwritten by config, will
+    # be checked for sanity before webserver start
+    app['MAX_REQUEST_SIZE'] = 5000000
+
+def init_aiohttp(conf):
+    loop = asyncio.get_event_loop()
+    app = web.Application(loop=loop)
+    app["CONF"] = conf
+    app['QUEUE'] = asyncio.Queue(maxsize=1)
+    app['LOOP'] = loop
+    return app
+
+async def handle_journal(request):
+    root = request.app['path-root']
+    full = os.path.join(root, "httpd/assets/webpage/journal.html")
+    with open(full, 'r') as content_file:
+        content = str.encode(content_file.read())
+        return web.Response(body=content, content_type='text/html')
+
+async def handle_utilization(request):
+    root = request.app['path-root']
+    full = os.path.join(root, "httpd/assets/webpage/utilization.html")
+    with open(full, 'r') as content_file:
+        content = str.encode(content_file.read())
+        return web.Response(body=content, content_type='text/html')
+
+async def handle_index(request):
+    # redirect -> raise web.HTTPFound('journal')
+    root = request.app['path-root']
+    full = os.path.join(request.app["CONF"]['page_dir'], 'index.html')
+    if not os.path.isfile(full):
+        return web.Response(text="NO DATA YET - SRY")
+    with open(full, 'r') as content_file:
+        content = str.encode(content_file.read())
+        return web.Response(body=content, content_type='text/html')
+
+def setup_routes(app, conf):
+    app.router.add_route('GET', '/api/v1/ping', api_ping.handle)
+
+    path_assets = os.path.join(app['path-root'], "httpd/assets")
+    app.router.add_static('/assets', path_assets, show_index=False)
+
+    app.router.add_get('/', handle_index)
+
+
+def timeout_daily_midnight(app):
+    log.debug("Execute daily execution handler")
+    try:
+        app['QUEUE'].put_nowait("328923923")
+    except:
+        print("the generator was already informed, ignore it, no need to regenerate")
+        pass
+
+def seconds_to_midnight():
+    return 5
+    now = datetime.datetime.now()
+    deltatime = datetime.timedelta(days=1)
+    tomorrow = datetime.datetime.replace(now + deltatime, hour=0, minute=0, second=0)
+    seconds = (tomorrow - now).seconds
+    if seconds < 60: return 60.0 # sanity checks
+    if seconds > 60 * 60 * 24: return 60.0 * 60 * 24
+    return seconds
+
+def register_timeout_handler_daily(app):
+    loop = asyncio.get_event_loop()
+    midnight_sec = seconds_to_midnight()
+    call_time = loop.time() + midnight_sec
+    msg = "Register daily timeout [scheduled in {} seconds]".format(midnight_sec)
+    log.debug(msg)
+    loop.call_at(call_time, register_timeout_handler_daily, app)
+    timeout_daily_midnight(app)
+
+
+def register_timeout_handler(app):
+    register_timeout_handler_daily(app)
+
+def setup_db(app):
+    app['path-root'] = os.path.dirname(os.path.realpath(__file__))
+
+async def recuring_memory_output(conf):
+    while True:
+        objects = pympler.muppy.get_objects()
+        sum1 = pympler.summary.summarize(objects)
+        pympler.summary.print_(sum1)
+        await asyncio.sleep(60)
+
+
+def init_debug_memory(conf):
+    log.err("initialize memory debugging")
+    if not pympler:
+        log.err("pympler not installed, memory_debug not possible")
+        return
+    asyncio.ensure_future(recuring_memory_output(conf))
+
+
+def init_debug(conf):
+    if 'memory_debug' in conf and conf['memory_debug']:
+        init_debug_memory(conf)
+
+def setup_page_dir(app):
+    if not 'page_dir' in app["CONF"]:
+        app["CONF"]['page_dir'] = tempfile.mkdtemp()
+    directory = app["CONF"]['page_dir']
+    if not directory.startswith('/'):
+        directory = os.path.join(app['path-root'], directory)
+        app["CONF"]['page_dir'] = directory
+    if os.path.exists(directory):
+        print("remove page dir: {}".format(directory))
+        shutil.rmtree(directory)
+    os.makedirs(directory, exist_ok=True)
+
+def setup_template_files(app):
+    if not 'template_dir' in app["CONF"]:
+        return
+    directory = app["CONF"]['template_dir']
+    if not os.path.exists(directory):
+        return
+    for filename in glob.glob(os.path.join(directory, '*.*')):
+        shutil.copy(filename, app["CONF"]['page_dir'])
+
+def setup_generator(app):
+    app['LOOP'].create_task(generator.generator(app))
+
+def main(conf):
+    init_debug(conf)
+    app = init_aiohttp(conf)
+    setup_db(app)
+    setup_page_dir(app)
+    setup_template_files(app)
+    setup_routes(app, conf)
+    register_timeout_handler(app)
+    setup_generator(app)
+    web.run_app(app, host="::", port=8080)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--configuration", help="configuration", type=str, default=None)
+    parser.add_argument("-v", "--verbose", help="verbose", action='store_true', default=False)
+    args = parser.parse_args()
+    if not args.configuration:
+        emsg = "Configuration required, please specify a valid file path, exiting now"
+        sys.stderr.write("{}\n".format(emsg))
+        emsg = "E.g.: \"./run.py -f assets/monetta.conf\""
+        sys.stderr.write("{}\n".format(emsg))
+        sys.exit(EXIT_FAILURE)
+    return args
+
+
+def load_configuration_file(args):
+    config = dict()
+    exec(open(args.configuration).read(), config)
+    return config
+
+def configuration_check(conf):
+    if not "host" in conf.common:
+        conf.common.host = '0.0.0.0'
+    if not "port" in conf.common:
+        conf.common.port = '8080'
+
+    if not "path" in conf.db:
+        sys.stderr.write("No path configured for database, but required! Please specify "
+                         "a path in db section\n")
+        sys.exit(EXIT_FAILURE)
+
+
+def init_logging(conf):
+    return
+    log_level_conf = "warning"
+    if conf.common.logging:
+        log_level_conf = conf.common.logging
+    numeric_level = getattr(logging, log_level_conf.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(numeric_level))
+    logging.basicConfig(level=numeric_level, format='%(message)s')
+    log.error("Log level configuration: {}".format(log_level_conf))
+
+
+def conf_init():
+    args = parse_args()
+    conf = load_configuration_file(args)
+    init_logging(conf)
+    return conf
+
+
+if __name__ == '__main__':
+    info_str = sys.version.replace('\n', ' ')
+    log.warning("Starting HIPID (python: {})".format(info_str))
+    conf = conf_init()
+    main(conf)
